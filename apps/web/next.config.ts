291d107fe9bab0da37dff67b7a300c3e822a7ed7
SaurabhJain708 <saurabhanchal381@gmail.com>
2025-08-08 02:26:41 +0530

    Tried to fix auth by changing nextconfig

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,14 @@
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 import type { NextConfig } from "next";
 
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config, { isServer }) {
+    if (isServer) {
+      config.plugins.push(new PrismaPlugin());
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
