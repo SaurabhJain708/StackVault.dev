952d304438e2b45eb9d3b935237e9d01751155d9
SaurabhJain708 <saurabhanchal381@gmail.com>
2025-08-10 17:38:44 +0530

    Added full profile flow now only small fixes left

--- apps/web/src/app/portfolio/[userId]/page.tsx
+++ apps/web/src/app/portfolio/[userId]/page.tsx
@@ -1,3 +1,23 @@
+import Spinner from "@/components/spinner";
+import dynamic from "next/dynamic";
+
+const AtlasTemplate = dynamic(() => import("@/components/templates/Atlas"), {
+  loading: () => <Spinner />,
+});
+const CanvasTemplate = dynamic(() => import("@/components/templates/Canvas"), {
+  loading: () => <Spinner />,
+});
+
+const HorizonTemplate = dynamic(
+  () => import("@/components/templates/Horizon"),
+  {
+    loading: () => <Spinner />,
+  },
+);
+const PulseTemplate = dynamic(() => import("@/components/templates/Pulse"), {
+  loading: () => <Spinner />,
+});
+
 async function getFullUser(userId: string) {
   const res = await fetch(
     `${process.env.NEXTAUTH_URL}/api/public?userid=${userId}`,
@@ -14,11 +34,21 @@ export default async function UserPage({
   const { userId } = await params;
   const userData = await getFullUser(userId);
 
+  let TemplateComponent = null;
+
+  if (userData.TemplateId === "140b76f2-b630-4546-bbf0-ae912ea5002b") {
+    TemplateComponent = <PulseTemplate data={userData} />;
+  } else if (userData.TemplateId === "4c3a1d73-dccc-4406-af9b-90546f601dd5") {
+    TemplateComponent = <CanvasTemplate data={userData} />;
+  } else if (userData.TemplateId === "9e1b1653-ee01-4f9f-835d-a437b4de87f5") {
+    TemplateComponent = <HorizonTemplate data={userData} />;
+  } else if (userData.TemplateId === "b53023bb-1fe3-4821-8e9a-318d51d93f1d") {
+    TemplateComponent = <AtlasTemplate data={userData} />;
+  } else {
+    return <div>Template not found</div>;
+  }
+
   if (!userData) return <div>User not found</div>;
 
-  return (
-    <div>
-      <h1>{userData.name}&apos;s Portfolio</h1>
-    </div>
-  );
+  return <div className="relative">{TemplateComponent}</div>;
 }
