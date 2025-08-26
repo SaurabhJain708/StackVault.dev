b4c78f726ffc4742d824ecaff69243050c1e2867
SaurabhJain708 <saurabhanchal381@gmail.com>
2025-08-26 13:09:01 +0530

    Fix

--- apps/web/src/middleware.ts
+++ apps/web/src/middleware.ts
@@ -47,7 +47,8 @@ export default withAuth(
           pathname === "/termsofservice" ||
           pathname === "/api/template" ||
           pathname === "/api/templatebyid" ||
-          pathname === "/"
+          pathname === "/" ||
+          pathname === "/api/getuserbydomain"
         ) {
           return true;
         }
