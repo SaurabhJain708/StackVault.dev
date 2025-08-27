e6af87d5619666bde0ae36c24748672c763204ac
SaurabhJain708 <saurabhanchal381@gmail.com>
2025-08-27 12:54:14 +0530

    Fixed

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -19,16 +19,18 @@ export default defineConfig({
     stdout: "pipe",
     stderr: "pipe",
     env: {
-      NODE_ENV: process.env.NODE_ENV!,
-      NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET!,
-      NEXTAUTH_URL: process.env.NEXTAUTH_URL!,
-      GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID!,
-      GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET!,
-      CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME!,
-      CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY!,
-      CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET!,
-      DATABASE_URL: process.env.DATABASE_URL!,
-      GEMINI_API_KEY: process.env.GEMINI_API_KEY!,
+      NODE_ENV: process.env.CI
+        ? "development"
+        : process.env.NODE_ENV || "development",
+      NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || "",
+      NEXTAUTH_URL: process.env.NEXTAUTH_URL || "",
+      GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "",
+      GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || "",
+      CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME || "",
+      CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY || "",
+      CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET || "",
+      DATABASE_URL: process.env.DATABASE_URL || "",
+      GEMINI_API_KEY: process.env.GEMINI_API_KEY || "",
     },
   },
 });
