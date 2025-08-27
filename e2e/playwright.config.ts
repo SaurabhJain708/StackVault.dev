8012bbfe65b92cfe06e321e6b3e36ef47be4551f
SaurabhJain708 <saurabhanchal381@gmail.com>
2025-08-27 14:41:41 +0530

    WIP: fix ci/cd

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -12,24 +12,9 @@ export default defineConfig({
     headless: true,
   },
   webServer: {
-    command: "bash ./test-start.sh",
-    timeout: 300 * 1000,
-    reuseExistingServer: false,
-    stdout: "pipe",
-    stderr: "pipe",
-    env: {
-      NODE_ENV: process.env.CI
-        ? "development"
-        : process.env.NODE_ENV || "development",
-      NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET || "",
-      NEXTAUTH_URL: process.env.NEXTAUTH_URL || "",
-      GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "",
-      GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || "",
-      CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME || "",
-      CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY || "",
-      CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET || "",
-      DATABASE_URL: process.env.DATABASE_URL || "",
-      GEMINI_API_KEY: process.env.GEMINI_API_KEY || "",
-    },
+    command: "",
+    port: 3000,
+    timeout: 240 * 1000,
+    reuseExistingServer: !process.env.CI,
   },
 });
